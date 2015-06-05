281a86b22d25b69cd626a6269e81f1e4a7669450
adamwu <calvinmankor@gmail.com>
2015-06-05 18:30:44 +0800

    *： proj file (win32、android)

--- proj.android/jni/Android.mk
+++ proj.android/jni/Android.mk
@@ -17,13 +17,25 @@ endef
 LOCAL_SRC_FILES := 	$(call all-cpp-files-under, main) \
 					$(call all-cpp-files-under, ../../Classes)
 
-LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes
+LOCAL_C_INCLUDES := $(LOCAL_PATH)/../../Classes \
+					$(LOCAL_PATH)/../../libs/libutils \
 
 LOCAL_WHOLE_STATIC_LIBRARIES += cocos2dx_static
-LOCAL_WHOLE_STATIC_LIBRARIES += ext_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocosdenshion_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_lua_static
+LOCAL_WHOLE_STATIC_LIBRARIES += box2d_static
+LOCAL_WHOLE_STATIC_LIBRARIES += chipmunk_static
+LOCAL_WHOLE_STATIC_LIBRARIES += cocos_extension_static
+LOCAL_WHOLE_STATIC_LIBRARIES += utils_static
 
 include $(BUILD_SHARED_LIBRARY)
 
 $(call import-module,cocos2dx)
-$(call import-module,extension)
+$(call import-module,CocosDenshion/android)
+$(call import-module,scripting/lua/proj.android)
+$(call import-module,cocos2dx/platform/third_party/android/prebuilt/libcurl)
+$(call import-module,extensions)
+$(call import-module,external/Box2D)
+$(call import-module,external/chipmunk)
+$(call import-module,libutils)
 
